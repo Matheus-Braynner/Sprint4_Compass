f133d706538258bd398c9444066e98221f8265dc
Matheus Braynner <m.braynner@outlook.com>
2022-03-18 15:36:00 -0300

    Project created

---
+++ politicians/src/main/java/com/compass/politicians/PoliticiansApplication.java
@@ -0,0 +1,13 @@
+package com.compass.politicians;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PoliticiansApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PoliticiansApplication.class, args);
+	}
+
+}
---
+++ politicians/src/test/java/com/compass/politicians/PoliticiansApplicationTests.java
@@ -0,0 +1,13 @@
+package com.compass.politicians;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PoliticiansApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
