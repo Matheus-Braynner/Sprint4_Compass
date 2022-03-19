c3bfc4ace0d5f376d06e6aa18241f63abbe20069
Matheus Braynner <m.braynner@outlook.com>
2022-03-19 17:21:40 -0300

    filter solved

--- politicians/src/main/java/com/compass/politicians/services/AssociateService.java
+++ politicians/src/main/java/com/compass/politicians/services/AssociateService.java
@@ -33,8 +33,8 @@ public class AssociateService {
 	}
 	
 	public List<Associate> findByPoliticalOffice(String politicalOffice) {
-		//return repository.findByPoliticalOffice(politicalOffice);
-		return null;
+		PoliticalOffice politicalOfficeEnum = PoliticalOffice.toEnum(politicalOffice.toUpperCase());
+		return repository.findByPoliticalOffice(politicalOfficeEnum);
 	}
 	
 	public Associate findById(Long id) {
