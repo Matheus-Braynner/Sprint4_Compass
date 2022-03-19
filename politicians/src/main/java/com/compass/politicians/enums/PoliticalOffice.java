d357d1b7c7c15f92481ef837eb294eac927913eb
Matheus Braynner <m.braynner@outlook.com>
2022-03-19 13:08:28 -0300

    bug

---
+++ politicians/src/main/java/com/compass/politicians/enums/PoliticalOffice.java
@@ -0,0 +1,12 @@
+package com.compass.politicians.enums;
+
+public enum PoliticalOffice {
+	COUNCILLOR,
+	MAYOR,
+	STATE_REPRESENTATIVE,
+	FEDERAL_REPRESENTATIVE,
+	SENATOR,
+	DISTRICT_GOVERNOR,
+	PRESIDENT,
+	NONE;
+}	
